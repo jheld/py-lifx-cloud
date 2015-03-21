b0763e8949656dc064687074827e802a0cf6a0fb
Jason Held <jsonheld@gmail.com>
2015-03-21 16:17:15 -0400

    method dimming over time, lifxbulb class.

---
+++ py_lifx_cloud/lifx_bulb.py
@@ -0,0 +1,8 @@
+__author__ = 'jason'
+
+class LifxBulb(object):
+    def __init__(self, bulb_id, bulb_name, bulb_group, is_on):
+        self.bulb_id = bulb_id
+        self.bulb_name = bulb_name
+        self.bulb_group = bulb_group
+        self.is_on = is_on
--- py_lifx_cloud/lifx_client.py
+++ py_lifx_cloud/lifx_client.py
@@ -3,13 +3,16 @@ import pycurl
 import io
 import json
 from urllib.parse import urlencode
+import time
+
+from lifx_bulb import LifxBulb
 
 
 class LifxClient(object):
     """
     Interface to manage LIFX bulbs through the cloud server.
     """
-    def __init__(self, client_token, server_version='v1beta1'):
+    def __init__(self, client_token, server_version='v1beta1',bulbs=[], find_bulbs=True):
         """
         Setup the client with credentials
         :param client_token: secret id for the client
@@ -21,6 +24,18 @@ class LifxClient(object):
         self.client_token = client_token
         self.credentials = '{}:'.format(self.client_token)
         self.lifx_server_url = 'https://api.lifx.com/{}/'.format(server_version)
+        self.bulbs = bulbs
+        if find_bulbs:
+            all_lights = self.request('lights/all')
+            for light in all_lights:
+                new_bulb = LifxBulb(light['id'], light['label'], light['group'], light['power'].lower() == 'on')
+                self.bulbs.append(new_bulb)
+            print(all_lights)
+
+    def dim_off(self, transition=1.0, bulbs=()):
+        # TODO: does not currently handle a different list of bulbs, in so far as making requests
+        fields = {'state': 'off', 'duration': transition}
+        print(self.request('lights/all/power', method='PUT', fields=fields))
 
     def request(self, sub_uri, method='GET', fields=None):
         buffer = io.BytesIO()
@@ -28,18 +43,35 @@ class LifxClient(object):
         c.setopt(pycurl.URL, '{}{}'.format(self.lifx_server_url, sub_uri))
         c.setopt(pycurl.USERPWD, self.credentials)
         c.setopt(pycurl.WRITEDATA, buffer)
-        if method == 'POST':
+        if method != 'GET':
             if fields is None:
                 fields = {}
             if not isinstance(fields, dict):
                 raise TypeError('Fields has to be a dict type.')
-            c.setopt(pycurl.POSTFIELDS, urlencode(fields))
+            encoded_fields= urlencode(fields)
+            c.setopt(pycurl.POSTFIELDS, encoded_fields)
+            c.setopt(pycurl.CUSTOMREQUEST, method)
         c.perform()
         body = buffer.getvalue().decode('utf-8')
         return json.loads(body)
+
+    def watch_dog_dim(self, bulbs=(), period=2700, transition=300):
+        """
+        Recurring watch dog "timer" to dim bulbs over time.
+
+        :param bulbs:
+        :param period:
+        :param transition:
+        :return:
+        """
+        # TODO: generalize this method to take a callable & args and rename to "watch_dog".
+        while True:
+            time.sleep(period)
+            self.dim_off(transition=transition, bulbs=bulbs)
 if __name__ == '__main__':
     print('Hello World')
     client_token = str(input('Client token: '))
     client = LifxClient(client_token=client_token)
-    print(client.request('lights/all'))
-    print(client.request('lights/label:Door/toggle', method='POST'))
+    # print(client.request('lights/all'))
+    # print(client.request('lights/label:Door/toggle', method='POST'))
+    client.watch_dog_dim()
